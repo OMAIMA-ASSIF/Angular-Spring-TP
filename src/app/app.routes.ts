cb90a6a909e92769027c8dbe2a48d058fb47ddc1
Omaima <omaima.assif-etu@etu.univh2c.ma>
2026-04-16 15:19:08 +0100

    Angular-spring tp

--- src/app/app.routes.ts
+++
@@ -1,8 +0,0 @@
-import { Routes } from '@angular/router';
-import {Home} from './home/home';
-import {Products} from './product/products';
-
-export const routes: Routes = [
-  {path : "home", component : Home},
-  {path : "prods", component : Products}
-];
